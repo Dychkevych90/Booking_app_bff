ba9edefb2bc481de676400edc39714a9d92b2901
mykola dychkevych <mykola.dychkevych@sigma.software>
2022-11-06 16:25:02 +0200

    added requests for User / create registration & login func

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const authRoute = require("./routes/auth");
 const hotelsRoute = require("./routes/hotels");
 const roomsRoute = require("./routes/auth");
 const usersRoute = require("./routes/auth");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 require("dotenv").config();
 const connection = require('./db');
@@ -13,12 +15,26 @@ const { API_PORT } = process.env;
 //connection to database
 connection();
 
+//middlewares
+app.use(cors());
+app.use(cookieParser())
 app.use(express.json());
 
 app.use('/api/auth', authRoute);
 app.use('/api/hotels', hotelsRoute);
 app.use('/api/rooms', roomsRoute);
 app.use('/api/users', usersRoute);
+
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Something went wrong!";
+  return res.status(errorStatus).json({
+    success: false,
+    status: errorStatus,
+    message: errorMessage,
+    stack: err.stack,
+  });
+});
 
 const port = API_PORT || 3001;
 app.listen(port, ()=> console.log(`Server running at ${port}`))
--- routes/hotels.js
+++ routes/hotels.js
@@ -1,55 +1,25 @@
-const Hotel = require('../models/hotels');
 const express = require("express");
+const {
+  createHotel,
+  updateHotel,
+  deleteHotel,
+  getHotel,
+  getHotels
+} = require("../controllers/hotels");
+
+const { verifyAdmin } = require("../utils/verifyToken");
 
 const router = express.Router();
 
 //CREATE
-router.post('/', async (req, res) => {
-
-  const newHotel = new Hotel(req.body)
-
-  try {
-    const savedHotel = await newHotel.save()
-    res.status(200).json(savedHotel)
-  } catch (err) {
-    res.status(500).json(err)
-  }
-});
+router.post("/",verifyAdmin, createHotel);
 //UPDATE
-router.put('/:id', async (req, res) => {
-  try {
-    const updateHotel = await Hotel.findByIdAndUpdate(req.params.id, {$set: req.body}, {new: true});
-    res.status(200).json(updateHotel)
-  } catch (err) {
-    res.status(500).json(err)
-  }
-});
+router.put("/:id",verifyAdmin, updateHotel);
 //DELETE
-router.delete('/:id', async (req, res) => {
-  try {
-    await Hotel.findByIdAndDelete(req.params.id);
-    res.status(200).json("Hotel has been deleted!")
-  } catch (err) {
-    res.status(500).json(err)
-  }
-});
+router.delete("/:id",verifyAdmin, deleteHotel);
 //GET
-router.get('/:id', async (req, res) => {
-  try {
-    const hotel = await Hotel.findById(req.params.id);
-    res.status(200).json(hotel)
-  } catch (err) {
-    res.status(500).json(err)
-  }
-});
+router.get("/find/:id", getHotel);
 //GET ALL
-router.get('/', async (req, res) => {
-  try {
-    const hotels = await Hotel.find();
-    res.status(200).json(hotels)
-  } catch (err) {
-    res.status(500).json(err)
-  }
-});
+router.get("/", getHotels);
 
 module.exports = router;
